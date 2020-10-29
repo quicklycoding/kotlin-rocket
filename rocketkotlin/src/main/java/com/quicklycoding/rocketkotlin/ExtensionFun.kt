3fd9ac57987f642d8f3401e2eb12406f3d094e65
Akash Dev <akash.dvlpr@gmail.com>
2020-10-29 10:32:46 +0530

    New files added

--- rocketkotlin/src/main/java/com/quicklycoding/rocketkotlin/ExtensionFun.kt
+++ rocketkotlin/src/main/java/com/quicklycoding/rocketkotlin/ExtensionFun.kt
@@ -8,28 +8,31 @@ import android.telephony.PhoneNumberUtils
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.Observer
-import com.google.android.material.snackbar.Snackbar
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
-//App packages
-const val PACKAGE_FACEBOOK = "com.facebook.katana"
-const val PACKAGE_WHATSAPP = "com.whatsapp"
-const val PACKAGE_TELEGRAM = "org.telegram.messenger"
-const val PACKAGE_INSTAGRAM = "com.instagram.android"
+/**
+ * Extension method to simplify view binding.
+ */
 
-fun String.toHTML() = HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY)
-
-fun <T> Context.openActivity(it: Class<T>, extras: Bundle.() -> Unit = {}) {
-    val intent = Intent(this, it)
-    intent.putExtras(Bundle().apply(extras))
-    startActivity(intent)
+fun Double.toRoundOffDecimal(): Double {
+    val df = DecimalFormat("#.##")
+    df.roundingMode = RoundingMode.FLOOR
+    return df.format(this).toDouble()
 }
+
+
+fun Context.hideSoftKeyboard(view: View) {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    if (imm.isAcceptingText) imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+val String.toHtml get() = HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_COMPACT)
+
+val <T : Any> T.TAG get() = this::class.simpleName
 
 // set arguments for DialogFragment
 inline fun <T : Fragment> T.withArgs(argsBuilder: Bundle.() -> Unit): T = this.apply {
@@ -51,30 +54,39 @@ fun Fragment.toast(message: Int, duration: Int = Toast.LENGTH_LONG) =
 
 
 // Copy
-fun Context.copyText(text: String) {
+fun Context.copy(text: String) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
     val clip = ClipData.newPlainText("", text)
     clipboard.setPrimaryClip(clip)
+    toast("Copy Successfully")
+}
+
+
+fun View.enable() {
+    isEnabled = true
+}
+
+fun View.disable() {
+    isEnabled = false
 }
 
 // Share
-fun Context.shareText(title: String, message: String, appPackage: String? = null) {
-    val intent = Intent(Intent.ACTION_SEND).apply {
-        type = "text/plain"
-        putExtra(Intent.EXTRA_TITLE, title)
-        putExtra(Intent.EXTRA_TEXT, message)
-    }
+fun share(context: Context, message: String, appId: String? = null) {
+//    Log.d("ExtensionFun", "share: $context, $message, $appId")
+
+    val intent = Intent(Intent.ACTION_SEND)
+    intent.type = "text/plain"
+    intent.putExtra(Intent.EXTRA_TEXT, message)
+    intent.putExtra(Intent.EXTRA_TITLE, message)
 
     when {
-        appPackage.isNullOrEmpty() -> {
-            startActivity(intent)
-        }
-        isPackageInstalled(appPackage) -> {
-            intent.setPackage(appPackage)
-            startActivity(intent)
-        }
+        appId.isNullOrEmpty() -> context.startActivity(intent)
+
         else -> {
-            toast("App not installed")
+            if (context.isPackageInstalled(appId)) {
+                intent.setPackage(appId)
+                context.startActivity(intent)
+            } else context.toast("App not installed")
         }
     }
 }
@@ -92,7 +104,7 @@ fun Context.isPackageInstalled(packageName: String): Boolean {
 
 // Send WhatsApp Message
 fun Context.specificWhatsAppMessage(mobile: String) {
-    if (isPackageInstalled(PACKAGE_WHATSAPP)) {
+    if (isPackageInstalled("com.whatsapp")) {
         Intent("android.intent.action.MAIN").apply {
             component = ComponentName("com.whatsapp", "com.whatsapp.Conversation")
             //phone number without "+" prefix
@@ -103,91 +115,32 @@ fun Context.specificWhatsAppMessage(mobile: String) {
 }
 
 
-fun Context.openDialer(mobile: String? = null) {
-    val intent = Intent(Intent.ACTION_DIAL)
-    intent.data = Uri.parse("tel:$mobile")
-    startActivity(intent)
+fun Context.dialer(mobile: String? = null) = Intent(Intent.ACTION_DIAL).run {
+    data = Uri.parse("tel:$mobile")
+    startActivity(this)
 }
 
-fun Context.openSMSApp(mobile: String?, message: String? = null) {
-    try {
-        val smsIntent = Intent(Intent.ACTION_VIEW)
-        smsIntent.type = "vnd.android-dir/mms-sms"
-        smsIntent.putExtra("address", "$mobile")
-        smsIntent.putExtra("sms_body", message)
-        startActivity(smsIntent)
-    } catch (e: ActivityNotFoundException) {
-    }
-
+fun sms(context: Context, mobile: String?, message: String? = null) = try {
+    val smsIntent = Intent(Intent.ACTION_VIEW, Uri.parse("sms:$mobile"))
+    smsIntent.putExtra("sms_body", message)
+    context.startActivity(smsIntent)
+} catch (e: ActivityNotFoundException) {
 }
 
 // Send Email
-fun Context.openGmailApp(email: String, subject: String) {
-    val intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", email, null))
-    intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-    intent.putExtra(Intent.EXTRA_TEXT, "")
-    startActivity(Intent.createChooser(intent, "Choose an Email client :"))
-}
-
-fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
-    supportFragmentManager.beginTransaction()
-        .add(frameId, fragment)
-        .commit()
-}
-
-fun Fragment.addFragment(fragment: Fragment, frameId: Int) {
-    childFragmentManager.beginTransaction()
-        .add(frameId, fragment)
-        .commit()
-}
-
-/*
-*
-* View Extension
-*
-* */
-
-// View Visible
-fun View.show() {
-    visibility = View.VISIBLE
-}
-
-// View Invisible
-fun View.hide() {
-    visibility = View.GONE
-}
-
-// hide keyboard
-fun View.hideKeyboard() {
-    val inputMethodManager =
-        context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    if (inputMethodManager.isAcceptingText) {
-        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+fun Context.email(emails: Array<String>, subject: String, message: String? = null) {
+    with(Intent(Intent.ACTION_SENDTO)) {
+        data = Uri.parse("mailto:")
+        putExtra(Intent.EXTRA_EMAIL, emails)
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, message)
+        if (resolveActivity(packageManager) != null) {
+            startActivity(this)
+        }
     }
 }
 
-//snackbar
-fun View.snackbar(message: String, duration: Int = Snackbar.LENGTH_LONG) =
-    Snackbar.make(this, message, duration).show()
 
-/*
-*
-*
-* LiveData : Extension
-*
-* */
-class NonNullMediatorLiveData<T> : MediatorLiveData<T>()
+//Open URL
+fun Context.openURL(url: String) = startActivity(Intent(Intent.ACTION_VIEW, url.toUri()))
 
-fun <T> LiveData<T>.nonNull(): NonNullMediatorLiveData<T> {
-    val mediator: NonNullMediatorLiveData<T> =
-        NonNullMediatorLiveData()
-    mediator.addSource(this) { it?.let { mediator.value = it } }
-    return mediator
-}
-
-
-fun <T> NonNullMediatorLiveData<T>.observe(owner: LifecycleOwner, observer: (t: T) -> Unit) {
-    this.observe(owner, Observer {
-        it?.let(observer)
-    })
-}
